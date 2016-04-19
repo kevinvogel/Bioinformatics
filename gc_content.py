98e05e61429686e15aaab68236438dfd22b69524
Kevin Vogel <kevinvogel@mac.com>
2016-04-19 19:29:43 -0400

    Create gc_content.py

---
+++ gc_content.py
@@ -0,0 +1,29 @@
+"""This function will calculate the GC% content of the given DNA sequence."""
+
+# Filename: /Users/kevinvogel/PycharmProjects/git_projects_bioinformatics/gc_content.py
+
+__author__ = 'Kevin Vogel'
+__email__ = 'kevinvogel@mac.com'
+
+
+def gc_function():
+    """
+    This function will calculate the GC% content of your desired sequence.
+    :return: GC percentage to the hundredth decimal place
+    """
+    # Randomly inserted DNA sequence.
+    seq = 'AAGCTCCTAATCACCCTCATAGCCTATAGTGATTCTCGACCTCGGTCAACTCTGCACGGCTTACAGGTAGGCCAGTTGACTAACTGTGGAGAATA'
+    # Counts the 'G' within the DNA sequence.
+    seq.count("G")
+    # Counts the 'C' within the DNA sequence.
+    seq.count("C")
+    # Length of the DNA sequence.
+    len(seq)
+    # Calculates the GC content of the DNA sequence.
+    gc_content = ((seq.count('G') + seq.count('C')) / len(seq)) * 100
+    # Prints the percentage to the console.
+    print('The GC percentage of this sequence is: ' + str(round(gc_content, 2)) + '%.')
+
+
+if __name__ == '__main__':
+    print(gc_function())
