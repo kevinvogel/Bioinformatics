de74ae61446cff83e5cc6e8891859c28c0ab3bb6
Kevin Vogel <kevinvogel@mac.com>
2016-04-20 18:28:08 -0400

    Update reverse_complement_sequence.py

--- reverse_complement_sequence.py
+++ reverse_complement_sequence.py
@@ -6,18 +6,16 @@ __author__ = 'Kevin Vogel'
 __email__ = 'kevinvogel@mac.com'
 
 
-def reverse_complement():
+def reverse_complement(seq):
     """
     This function will display the reverse complement of a DNA sequence.
     :return: reverse complement of the DNA sequence
     """
-    # Randomly inserted DNA sequence.
-    seq = 'AAGCTCCTAATCACCCTCATAGCCTATAGTGATTCTCGACCTCGGTCAACTCTGCACGGCTTACAGGTAGGCCAGTTGACTAACTGTGGAGAATA'
     # Dictionary to store the base pairs.
-    seq_dict = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    seq_dict = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G', 'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
     # Replaces each nucleotide with its base pair partner and reverses the sequence.
     return "".join([seq_dict[base] for base in reversed(seq)])
 
 
 if __name__ == '__main__':
-    print(reverse_complement())
+    print(reverse_complement(seq))
